bdea2286b6ec358be0636dc7b36fa362a556051b
Fabricio Dias <fabricio270909@gmail.com>
2025-01-17 14:09:23 -0300

    Try - catch example

---
+++ DemoTryCatch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DemoTryCatch
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Division by zero isn't allowed");
+                /*Console.WriteLine($"Error: {e.Message}");*/
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format error: {e.Message}");
+            }
+
+        }
+    }
+}
+
